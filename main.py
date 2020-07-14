3a19c5fa7ab496aff46ca5f584a0255e57fd080d
abumaz99 <46001820+abumaz99@users.noreply.github.com>
2020-07-14 18:49:01 +0100

    Creating model and evaluating it.

--- main.py
+++ main.py
@@ -25,3 +25,68 @@ def load_data(csv_file, split=0.9):
     return texts[:split], train_labels, texts[split:], val_labels
 
 train_texts, train_labels, val_texts, val_labels = load_data(df)
+
+nlp = spacy.blank("en")
+
+textcat = nlp.create_pipe(
+    "textcat",
+    config={
+        "exclusive_classes":True,
+        "architecture":"bow"
+    }
+)
+
+nlp.add_pipe(textcat)
+
+textcat.add_label("NEGATIVE")
+textcat.add_label("POSITIVE")
+
+from spacy.util import minibatch
+
+def train(model, train_data, optimizer):
+    losses ={}
+    random.seed(1)
+    random.shuffle(train_data)
+
+    batches = minibatch(train_data, size=8)
+    for batch in batches:
+        texts, labels = zip(*batch)
+
+    model.update(texts, labels, sgd=optimizer, losses=losses)
+
+    return losses
+
+spacy.util.fix_random_seed(1)
+random.seed(1)
+
+optimizer = nlp.begin_training()
+train_data = list(zip(train_texts, train_labels))
+losses = train(nlp, train_data, optimizer)
+
+def predict(model, texts):
+    docs = [nlp.tokenizer(text) for text in texts]
+
+    textcat = model.get_pipe('textcat')
+    scores, _ = textcat.predict(docs)
+
+    predicted_class = scores.argmax(axis=1)
+
+    return predicted_class
+
+def evaluate(model, texts, labels):
+    predicted_class = predict(model, texts)
+
+    true_class = [int(each['cats']['POSITIVE'])for each in labels]
+
+    correct_predictions = predicted_class == true_class
+
+    accuracy = correct_predictions.mean()
+
+    return accuracy
+
+n_iters = 5
+for i in range(n_iters):
+    losses = train(nlp, train_data, optimizer)
+    accuracy = evaluate(nlp, val_texts, val_labels)
+    print(f"Loss: {losses['textcat']:.3f} \t Accuracy: {accuracy:.3f}")
+
